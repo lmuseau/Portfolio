c6bc7a013f951ac79f93782abdc51b7bd67359fa
Leonard Museau <lmuseau@gmail.com>
2020-10-09 11:20:01 -0500

    fixed typo in text

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: "Hello! I'm a Missouri-based full stack developer. I have 2.5 years of experience working for a small company that specializes data integrations to improve healthcare providers and payers. I also enjoy working on open source projects and developing websites for different clients.",
+  paragraphOne: "Hello! I'm a Missouri-based full stack developer. I have 2.5 years of experience working for a small company that specializes in data integrations to improve healthcare providers and payers. I also enjoy working on open source projects and developing websites for different clients.",
   paragraphTwo: "I have a diverse set of skills ranging from AngularJS and ReactJS with Javascript + HTML + CSS to using RESTful APIs with NodeJS and MySQL. I also have some exposure to AWS, Docker, Kubenetes, and so on.",
   paragraphThree: 'Check out my resume below for a full list of technologies I have interacted with.',
   resume: 'https://www.linkedin.com/in/leonard-museau/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAACt6sL0BaoA1v8_XdDNn1fVbK5UStngTbO4,1600379278992)/', // if no resume, the button will not show up
