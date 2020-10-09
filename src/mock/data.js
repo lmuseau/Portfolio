7231aea76bf6e292807c858543b350551d71c0da
Leonard Museau <lmuseau@gmail.com>
2020-10-08 19:00:13 -0500

    redesigned the website

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,46 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Leonard | Full Stack Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Welcome! I am',
+  name: 'Leonard Museau',
+  subtitle: 'Full Stack Developer',
+  cta: 'About Me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: "Hello! I'm a Missouri-based full stack developer. I have 2.5 years of experience working for a small company that specializes data integrations to improve healthcare providers and payers. I also enjoy working on open source projects and developing websites for different clients.",
+  paragraphTwo: "I have a diverse set of skills ranging from AngularJS and ReactJS with Javascript + HTML + CSS to using RESTful APIs with NodeJS and MySQL. I also have some exposure to AWS, Docker, Kubenetes, and so on.",
+  paragraphThree: 'Check out my resume below for a full list of technologies I have interacted with.',
+  resume: 'https://www.linkedin.com/in/leonard-museau/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAACt6sL0BaoA1v8_XdDNn1fVbK5UStngTbO4,1600379278992)/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'fa-screenshot.png',
+    title: 'St. Robert First Assembly of God Website',
+    info: 'My old home church website built using ReactJS. It is currently being developed right now.',
+    info2: "Will release the full url to the site when it gets deployed.",
+    url: 'https://github.com/lmuseau/st-robert-first-assembly',
+    repo: 'https://github.com/lmuseau/st-robert-first-assembly', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: "I'd be happy to connect and work with you!",
+  btn: "Let's Chat",
+  email: 'lmuseau@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +50,27 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://www.twitter.com/leonardmuseau/',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'facebook',
+      url: 'https://www.facebook.com/leonardmuseau/',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/leonard-museau/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/lmuseau/',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
