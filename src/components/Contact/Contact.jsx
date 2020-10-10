5349d53b17ffdbcca951edda5e76e9e326771a06
Leonard Museau <lmuseau@gmail.com>
2020-10-10 12:58:53 -0500

    made button an input

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,10 +3,11 @@ import Fade from 'react-reveal/Fade';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
+import './Contact.css';
 
 const Contact = () => {
   const { contact } = useContext(PortfolioContext);
-  const { cta, btn, email } = contact;
+  const { cta } = contact;
 
   return (
     <section id="contact">
@@ -14,17 +15,47 @@ const Contact = () => {
         <Title title="Contact" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-            <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
-            </p>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
-            >
-              {btn || "Let's Talk"}
-            </a>
+            <form action="POST" data-netlify="true" name="contact" className="contact-box">
+              <div className="content">
+                {/* <h2>Let's Work</h2> */}
+                <input
+                  name="name"
+                  id="name"
+                  type="text"
+                  className="field"
+                  placeholder="Your Name"
+                />
+                <input
+                  name="email"
+                  id="email"
+                  type="email"
+                  className="field"
+                  placeholder="Your Email"
+                />
+                <input
+                  name="phone"
+                  id="phone"
+                  type="text"
+                  className="field"
+                  placeholder="Your Phone"
+                />
+                <textarea
+                  name="message"
+                  id="message"
+                  className="field area"
+                  placeholder="Message"
+                />
+                <div className="field">
+                  <div data-netlify-recaptcha="true" />
+                </div>
+                <input type="submit" value="Send" className="btn" />
+              </div>
+              <div className="">
+                <p className="contact-wrapper__text">
+                  {cta || 'Would you like to work with me? Awesome!'}
+                </p>
+              </div>
+            </form>
           </div>
         </Fade>
       </Container>
