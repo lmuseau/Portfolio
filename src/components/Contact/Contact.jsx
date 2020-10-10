4929d0943a16eafc19f4c6cf2be10b07066a43b4
Leonard Museau <lmuseau@gmail.com>
2020-10-10 15:43:06 -0500

    form handling with a stateful react form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
@@ -9,6 +9,38 @@ const Contact = () => {
   const { contact } = useContext(PortfolioContext);
   const { cta } = contact;
 
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&');
+  };
+
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact3', ...formState }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
   return (
     <section id="contact">
       <Container>
@@ -17,19 +49,21 @@ const Contact = () => {
           <div className="contact-wrapper">
             <div className="contact-box">
               <form
-                name="contact2"
+                name="contact3"
                 method="post"
                 data-netlify="true"
-                onSubmit="submit"
-                data-netlify-recaptcha="true"
+                onSubmit={handleSubmit}
+                data-netlify-honeypot="bot-field"
               >
                 {/* You still need to add the hidden input with the form name to your JSX form */}
-                <input type="hidden" name="form-name" value="contact2" />
+                <input type="hidden" name="form-name" value="contact3" />
                 <div className="content">
                   <input
                     name="name"
                     id="name"
                     type="text"
+                    onChange={handleChange}
+                    value={formState.name}
                     className="field"
                     placeholder="Your Name"
                   />
@@ -37,6 +71,8 @@ const Contact = () => {
                     name="email"
                     id="email"
                     type="email"
+                    onChange={handleChange}
+                    value={formState.email}
                     className="field"
                     placeholder="Your Email"
                   />
@@ -44,12 +80,16 @@ const Contact = () => {
                     name="phone"
                     id="phone"
                     type="text"
+                    onChange={handleChange}
+                    value={formState.phone}
                     className="field"
                     placeholder="Your Phone"
                   />
                   <textarea
                     name="message"
                     id="message"
+                    onChange={handleChange}
+                    value={formState.message}
                     className="field area"
                     placeholder="Message"
                   />
