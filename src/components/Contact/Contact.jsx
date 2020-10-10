71458fc23f25a28533ed93eff8e6d7bd249e0249
Leonard Museau <lmuseau@gmail.com>
2020-10-10 14:20:04 -0500

    maybe this will fix my form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,53 +15,54 @@ const Contact = () => {
         <Title title="Contact" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-            <form
-              method="post"
-              name="contact"
-              className="contact-box"
-              data-netlify-recaptcha="true"
-              data-netlify="true"
-            >
-              {/* You still need to add the hidden input with the form name to your JSX form */}
-              <input type="hidden" name="form-name" value="contact" />
-              <div className="content">
-                {/* <h2>Let's Work</h2> */}
-                <input
-                  name="name"
-                  id="name"
-                  type="text"
-                  className="field"
-                  placeholder="Your Name"
-                />
-                <input
-                  name="email"
-                  id="email"
-                  type="email"
-                  className="field"
-                  placeholder="Your Email"
-                />
-                <input
-                  name="phone"
-                  id="phone"
-                  type="text"
-                  className="field"
-                  placeholder="Your Phone"
-                />
-                <textarea
-                  name="message"
-                  id="message"
-                  className="field area"
-                  placeholder="Message"
-                />
-                <div data-netlify-recaptcha="true" />
-                <button type="submit" className="btn">
-                  Send
-                </button>
-              </div>
+            <div className="contact-box">
+              <form
+                name="contact1"
+                method="post"
+                data-netlify="true"
+                onSubmit="submit"
+                data-netlify-recaptcha="true"
+              >
+                {/* You still need to add the hidden input with the form name to your JSX form */}
+                <input type="hidden" name="form-name" value="contact1" />
+                <div className="content">
+                  <input
+                    name="name"
+                    id="name"
+                    type="text"
+                    className="field"
+                    placeholder="Your Name"
+                  />
+                  <input
+                    name="email"
+                    id="email"
+                    type="email"
+                    className="field"
+                    placeholder="Your Email"
+                  />
+                  <input
+                    name="phone"
+                    id="phone"
+                    type="text"
+                    className="field"
+                    placeholder="Your Phone"
+                  />
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="field area"
+                    placeholder="Message"
+                  />
+                  <div data-netlify-recaptcha="true" />
+                  <button type="submit" className="btn">
+                    Send
+                  </button>
+                </div>
+              </form>
               <div className="">
                 <p className="contact-wrapper__text">{cta}</p>
               </div>
-            </form>
+            </div>
           </div>
         </Fade>
       </Container>
