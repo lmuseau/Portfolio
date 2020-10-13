71b8ae91e76edf74a9ac05845a24eca9f420055d
Leonard Museau <lmuseau@gmail.com>
2020-10-13 10:17:40 -0500

    updated my projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,7 +4,7 @@ import { nanoid } from 'nanoid';
 export const headData = {
   title: 'Leonard | Full Stack Developer', // e.g: 'Name | Developer'
   lang: 'en', // e.g: en, es, fr, jp
-  description: 'Welcome to my website', // e.g: Welcome to my website
+  description: 'Portfolio of Leonard Museau', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -18,10 +18,13 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: "Hello! I'm a Missouri-based full stack developer. I have 2.5 years of experience working for a small company that specializes in data integrations to improve healthcare providers and payers. I also enjoy working on open source projects and developing websites for different clients.",
-  paragraphTwo: "I have a diverse set of skills ranging from AngularJS and ReactJS with Javascript + HTML + CSS to using RESTful APIs with NodeJS and MySQL. I also have some exposure to AWS, Docker, Kubenetes, and so on.",
-  paragraphThree: 'Check out my resume below for a full list of technologies I have interacted with.',
-  resume: 'https://www.linkedin.com/in/leonard-museau/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAACt6sL0BaoA1v8_XdDNn1fVbK5UStngTbO4,1600379278992)/', // if no resume, the button will not show up
+  paragraphOne:
+    "Hello! I'm a Missouri-based full stack developer. I have 2.5 years of experience working for a small company that specializes in data integrations to improve healthcare providers and payers. I also enjoy working on open source projects and developing websites for different clients.",
+  paragraphTwo:
+    'I have a diverse set of skills ranging from AngularJS and ReactJS with Javascript + HTML + CSS to using RESTful APIs with NodeJS and MySQL. I also have some exposure to AWS, Docker, Kubenetes, and so on.',
+  paragraphThree:
+    'My LinkedIn has a resume so you can view the full list of technologies I have interacted with.',
+  resume: 'https://www.linkedin.com/in/leonard-museau/',
 };
 
 // PROJECTS DATA
@@ -30,10 +33,19 @@ export const projectsData = [
     id: nanoid(),
     img: 'fa-screenshot.png',
     title: 'St. Robert First Assembly of God Website',
-    info: 'My old home church website built using ReactJS. It is currently being developed right now.',
-    info2: "Will release the full url to the site when it gets deployed.",
-    url: 'https://github.com/lmuseau/st-robert-first-assembly',
+    info: 'This is a church website built using ReactJS. It is currently in development.',
+    info2: 'Back end technologies will be implemented soon.',
+    url: 'https://quizzical-williams-74d0e2.netlify.app/',
     repo: 'https://github.com/lmuseau/st-robert-first-assembly', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'fa-snake-screenshot.png',
+    title: 'Snake Game',
+    info: 'This is my implementation of a popular game, Snake, built using only React JS.',
+    info2: "This project showcases my understanding of React's component class and their API's.",
+    url: 'https://musing-jepsen-3c242a.netlify.app',
+    repo: 'https://github.com/lmuseau/snake-game', // if no repo, the button will not show up
   },
 ];
 
